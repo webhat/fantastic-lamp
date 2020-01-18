fac9f73d9f35871444cbd64a1cd68452e9d25e17
Daniël W. Crompton <daniel.crompton@gmail.com>
2020-01-18 12:34:13 +0100

    Setup go modules for running in docker

--- main.go
+++ main.go
@@ -20,106 +20,48 @@ limitations under the License.
 package main
 
 import (
-	"expvar"
+	roman "fantastic-lamp/task2"
 	"flag"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"sync"
-	"time"
 )
 
 // Command-line flags.
 var (
-	httpAddr   = flag.String("http", ":8080", "Listen address")
-	pollPeriod = flag.Duration("poll", 5*time.Second, "Poll period")
-	version    = flag.String("version", "1.4", "Go version")
+	httpAddr = flag.String("http", ":8080", "Listen address")
 )
-
-const baseChangeURL = "https://go.googlesource.com/go/+/"
 
 func main() {
 	flag.Parse()
-	changeURL := fmt.Sprintf("%sgo%s", baseChangeURL, *version)
-	http.Handle("/", NewServer(*version, changeURL, *pollPeriod))
+	http.Handle("/", NewServer())
 	log.Fatal(http.ListenAndServe(*httpAddr, nil))
 }
-
-// Exported variables for monitoring the server.
-// These are exported via HTTP as a JSON object at /debug/vars.
-var (
-	hitCount       = expvar.NewInt("hitCount")
-	pollCount      = expvar.NewInt("pollCount")
-	pollError      = expvar.NewString("pollError")
-	pollErrorCount = expvar.NewInt("pollErrorCount")
-)
 
 // Server implements the outyet server.
 // It serves the user interface (it's an http.Handler)
 // and polls the remote repository for changes.
 type Server struct {
-	version string
-	url     string
-	period  time.Duration
-
-	mu  sync.RWMutex // protects the yes variable
-	yes bool
+	Expr string
 }
 
 // NewServer returns an initialized outyet server.
-func NewServer(version, url string, period time.Duration) *Server {
-	s := &Server{version: version, url: url, period: period}
-	go s.poll()
+func NewServer() *Server {
+	s := &Server{}
 	return s
-}
-
-// poll polls the change URL for the specified period until the tag exists.
-// Then it sets the Server's yes field true and exits.
-func (s *Server) poll() {
-	for !isTagged(s.url) {
-		pollSleep(s.period)
-	}
-	s.mu.Lock()
-	s.yes = true
-	s.mu.Unlock()
-	pollDone()
-}
-
-// Hooks that may be overridden for integration tests.
-var (
-	pollSleep = time.Sleep
-	pollDone  = func() {}
-)
-
-// isTagged makes an HTTP HEAD request to the given URL and reports whether it
-// returned a 200 OK response.
-func isTagged(url string) bool {
-	pollCount.Add(1)
-	r, err := http.Head(url)
-	if err != nil {
-		log.Print(err)
-		pollError.Set(err.Error())
-		pollErrorCount.Add(1)
-		return false
-	}
-	return r.StatusCode == http.StatusOK
 }
 
 // ServeHTTP implements the HTTP user interface.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	hitCount.Add(1)
-	s.mu.RLock()
+	s.Expr = "XI+I"
 	data := struct {
-		URL     string
-		Version string
-		Yes     bool
+		Expr  string
+		Value string
 	}{
-		s.url,
-		s.version,
-		s.yes,
+		s.Expr,
+		//	"XI",
+		roman.RomanNumeralCalculate(s.Expr),
 	}
-	s.mu.RUnlock()
 	err := tmpl.Execute(w, data)
 	if err != nil {
 		log.Print(err)
@@ -129,13 +71,9 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // tmpl is the HTML template that drives the user interface.
 var tmpl = template.Must(template.New("tmpl").Parse(`
 <!DOCTYPE html><html><body><center>
-	<h2>Is Go {{.Version}} out yet?</h2>
+	<h2>{{.Expr}}</h2>
 	<h1>
-	{{if .Yes}}
-		<a href="{{.URL}}">YES!</a>
-	{{else}}
-		No. :-(
-	{{end}}
+	{{.Value}}
 	</h1>
 </center></body></html>
 `))
